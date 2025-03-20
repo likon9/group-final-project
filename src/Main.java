c56e5cde2e1deed7f9e1ed688e74ca19ba0eebdf
Alex <likonpro2001@gmail.com>
2025-03-20 18:36:11 +0300

    dev

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,14 @@
+import Entity.Animal;
+import Entity.AnimalEyeColor;
+import Sorting.QuickSort;
+
+import java.util.ArrayList;
+import java.util.List;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
     }
 }
