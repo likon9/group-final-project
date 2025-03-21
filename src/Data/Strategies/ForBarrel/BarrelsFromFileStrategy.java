8286a4ded642c527d16a011c48086dde8cf88a6c
antonshynkevich <anton.shynkevich@gmail.com>
2025-03-21 21:06:26 +0300

    Add some test data. Add JSON data serialization service. Fix some typos. Change the strategy pattern a little.

--- src/Data/Strategies/ForBarrel/BarrelsFromFileStrategy.java
+++ src/Data/Strategies/ForBarrel/BarrelsFromFileStrategy.java
@@ -1,27 +1,12 @@
 package Data.Strategies.ForBarrel;
 
-import Data.Services.JsonService;
-import Data.Services.MappingService;
 import Data.Strategies.ItemTypeStrategy;
-import Data.VeiwModels.BarrelViewModel;
-import Entity.Barrel;
-import Entity.Item;
 
 import java.util.List;
 
 public class BarrelsFromFileStrategy implements ItemTypeStrategy {
     @Override
-    public List<Item> getCollection(int collectionLength) {
-        var path = "CollectionFiles/barrels.json";
-        var jsonService = new JsonService<BarrelViewModel>();
-        List<BarrelViewModel> barrelViewModels = jsonService.readJson(path, BarrelViewModel.class);
-        var mappingService = new MappingService();
-
-        return barrelViewModels.stream()
-                .map(mappingService::BarrelViewModelToBarrel)
-                .filter(Barrel::isValid)
-                .limit(collectionLength)
-                .map(a -> (Item)a)
-                .toList();
+    public List<Comparable<?>> getCollection(int collectionLength) {
+        return List.of();
     }
 }
--- src/Data/Strategies/ForPerson/PersonsFromFileStrategy.java
+++ src/Data/Strategies/ForPerson/PersonsFromFileStrategy.java
@@ -1,26 +1,21 @@
 package Data.Strategies.ForPerson;
 
-import Data.Services.JsonService;
-import Data.Services.MappingService;
 import Data.Strategies.ItemTypeStrategy;
-import Data.VeiwModels.PersonViewModel;
-import Entity.Item;
+import Entity.Gender;
 import Entity.Person;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PersonsFromFileStrategy implements ItemTypeStrategy {
     @Override
-    public List<Item> getCollection(int collectionLength) {
-        var jsonService = new JsonService<PersonViewModel>();
-        var mappingService = new MappingService();
-        var path = "CollectionFiles/persons.json";
-        List<PersonViewModel> personViewModels = jsonService.readJson(path, PersonViewModel.class);
-        return personViewModels.stream()
-                .map(mappingService::PersonViewModelToPerson)
-                .filter(Person::isValid)
-                .limit(collectionLength)
-                .map(a -> (Item)a)
-                .toList();
+    public List<Comparable<?>> getCollection(int collectionLength) {
+        List<Comparable<?>> list = new ArrayList<>();
+        var personBuilder = new Person.PersonBuilder(Gender.MALE);
+        personBuilder.setAge(29);
+        personBuilder.setLastName("Shynkevich");
+        Person person = personBuilder.build();
+        list.add(person);
+        return list;
     }
 }
