4c097b39cad91aad3f6f1789486668bfc3fcf001
RUSemp <44526428+RUSemp@users.noreply.github.com>
2025-03-21 19:56:58 +0400

    List interface realized

--- src/customlist/CustomArrayList.java
+++ src/customlist/CustomArrayList.java
@@ -1,6 +1,8 @@
 package customlist;
 
-public class CustomArrayList<T>{
+import java.util.*;
+
+public class CustomArrayList<T> implements List<T> {
     private final int INIT_SIZE = 16;
     private int capacity;
     private Object[] array;
@@ -16,26 +18,69 @@ public class CustomArrayList<T>{
         array = new Object[capacity];
     }
 
+    @Override
     public int size(){
         return size;
     }
 
+    @Override
     public boolean isEmpty() {
         return size == 0;
     }
 
+    @Override
     public boolean contains(Object element) {
         return indexOf(element) >= 0;
     }
 
-    public void add(T item) {
+    @Override
+    public Iterator<T> iterator() {
+        Iterator<T> it = new Iterator<T>() {
+            private int currentIndex = 0;
+
+            @Override
+            public boolean hasNext() {
+                return currentIndex < size && array[currentIndex] != null;
+            }
+
+            @Override
+            public T next() {
+                return (T) array[currentIndex++];
+            }
+        };
+        return it;
+    }
+
+    @Override
+    public Object[] toArray() {
+        Object[] newArray = new Object[size];
+        System.arraycopy(array, 0, newArray, 0, size);
+        return newArray;
+    }
+
+    @Override
+    public <T1> T1[] toArray(T1[] a) {
+        if (a.length < size) {
+            return (T1[]) Arrays.copyOf(array, size, a.getClass());
+        }
+        System.arraycopy(array,0, a, 0, size);
+        if (a.length > size) {
+            a[size] = null;
+        }
+        return a;
+    }
+
+    @Override
+    public boolean add(Object item) {
         if (size == capacity) {
             resize();
         }
         array[size++] = item;
+        return true;
     }
 
-    public void add(int index, T item) {
+    @Override
+    public void add(int index, Object item) {
         if (index < 0) {
             return;
         }
@@ -65,6 +110,7 @@ public class CustomArrayList<T>{
         return null;
     }
 
+    @Override
     public T remove(int index) {
         Object o = null;
         if (index < size && index >= 0) {
@@ -74,6 +120,7 @@ public class CustomArrayList<T>{
         return (T) o;
     }
 
+    @Override
     public boolean remove(Object element) {
         if(size == 0) {
             return false;
@@ -94,6 +141,100 @@ public class CustomArrayList<T>{
         return false;
     }
 
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        if (c == null){
+            return false;
+        }
+        if (c.isEmpty()) {
+            return true;
+        }
+        for (Object e : c) {
+            if (contains(e)) {
+                ;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends T> c) {
+        if (c == null) {
+            return false;
+        }
+        if (c.isEmpty()) {
+            return false;
+        }
+        for (Object item : c) {
+            add(item);
+        }
+        return true;
+    }
+
+    @Override
+    public boolean addAll(int index, Collection<? extends T> c) {
+        if (c == null) {
+            return false;
+        }
+        if (c.isEmpty() || index < 0) {
+            return false;
+        }
+        if (index > size) {
+            index = size;
+        }
+        for (Object item : c) {
+            add(index++, item);
+        }
+
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        if (c == null) {
+            return false;
+        }
+        if (c.isEmpty() || size == 0) {
+            return false;
+        }
+        boolean modyfied = false;
+        int i = 0;
+        while (i < size) {
+            if (c.contains(array[i])) {
+                shiftToLeft(i);
+                modyfied = true;
+            } else {
+                i++;
+            }
+        }
+        return modyfied;
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        if (c == null) {
+            return true;
+        }
+        if (c.isEmpty()) {
+            clear();
+            return true;
+        }
+        boolean modyfied = false;
+        int i = 0;
+        while (i < size) {
+            if (c.contains(array[i])) {
+                i++;
+            } else {
+                shiftToLeft(i);
+                modyfied = true;
+            }
+        }
+        return modyfied;
+    }
+
+    @Override
     public void clear() {
         for (int i = 0; i < size; i++) {
             array[i] = null;
@@ -119,6 +260,7 @@ public class CustomArrayList<T>{
         array[size] = null;
     }
 
+    @Override
     public int indexOf(Object o) {
         if (o == null) {
             for (int i = 0; i < size; i++) {
@@ -134,5 +276,52 @@ public class CustomArrayList<T>{
             }
         }
         return -1;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        int lastIndex = -1;
+        if (o == null) {
+            for (int i = 0; i < size; i++) {
+                if (array[i] == null) {
+                    lastIndex = i;
+                }
+            }
+            return lastIndex;
+        } else {
+            for (int i = 0; i < size; i++) {
+                if (o.equals(array[i])) {
+                    lastIndex = i;
+                }
+            }
+        }
+        return lastIndex;
+    }
+
+    @Override
+    public ListIterator<T> listIterator() {
+        return null;
+    }
+
+    @Override
+    public ListIterator<T> listIterator(int index) {
+        return null;
+    }
+
+    @Override
+    public List<T> subList(int fromIndex, int toIndex) {
+        if (fromIndex > toIndex) {
+            int tmp = fromIndex;
+            fromIndex = toIndex;
+            toIndex = tmp;
+        }
+        if (fromIndex < 0 || toIndex > size) {
+            return null;
+        }
+        List<T> customArrayList = new CustomArrayList<>(toIndex - fromIndex);
+        for (int i = fromIndex; i < toIndex; i++) {
+            customArrayList.add((T) array[i]);
+        }
+        return customArrayList;
     }
 }
