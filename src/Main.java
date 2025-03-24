a8fe8ee6fac70f00ab09b2676ede48caa18f5e0e
antonshynkevich <anton.shynkevich@gmail.com>
2025-03-24 11:20:43 +0300

    Add the Item interface to provide a common interface for the all 3 entities. Add isValid() method to all entities to provide objects validation during loading data from different sources.

--- src/Main.java
+++ src/Main.java
@@ -1,53 +1,12 @@
-//
-// Source code recreated from a .class file by IntelliJ IDEA
-// (powered by FernFlower decompiler)
-//
-
 import Entity.Animal;
-import Entity.Gender;
-import Entity.Person;
-import Sorting.InsertionSort;
-import Customlist.CustomArrayList;
-import java.io.PrintStream;
-import java.util.Objects;
-import java.util.stream.Stream;
+import Entity.AnimalEyeColor;
+import Sorting.QuickSort;
+import customlist.CustomArrayList;
 
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public Main() {
-    }
-
     public static void main(String[] args) {
-        Animal animal1 = (new Animal.AnimalBuilder("Dog")).build();
-        Animal animal2 = (new Animal.AnimalBuilder("Cat")).build();
-        CustomArrayList<Animal> animals = new CustomArrayList();
-        animals.add(animal1);
-        animals.add(animal2);
-        Person person1 = (new Person.PersonBuilder(Gender.FEMALE)).build();
-        Person person2 = (new Person.PersonBuilder(Gender.MALE)).build();
-        CustomArrayList<Person> persons = new CustomArrayList();
-        persons.add(person1);
-        persons.add(person2);
-        Stream var10000 = animals.stream();
-        PrintStream var10001 = System.out;
-        Objects.requireNonNull(var10001);
-        var10000.forEach(var10001::println);
-        System.out.println();
-        var10000 = persons.stream();
-        var10001 = System.out;
-        Objects.requireNonNull(var10001);
-        var10000.forEach(var10001::println);
-        System.out.println();
-        InsertionSort.insertionSort(animals);
-        InsertionSort.insertionSort(persons);
-        var10000 = animals.stream();
-        var10001 = System.out;
-        Objects.requireNonNull(var10001);
-        var10000.forEach(var10001::println);
-        System.out.println();
-        var10000 = persons.stream();
-        var10001 = System.out;
-        Objects.requireNonNull(var10001);
-        var10000.forEach(var10001::println);
-        System.out.println();
+
     }
 }
